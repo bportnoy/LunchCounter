d6bc55bd6e6c6a1097f3faffc77c271a733235bf
Bradley Portnoy <bradley.portnoy@gmail.com>
2015-03-27 22:51:07 -0700

    Scaffold app with Angular Fullstack generator.

---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('lunchCounterApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
